5330fa3270145d4a84394873f26a75c95d8eabe7
SamiAyala <samirayala23@gmail.com>
2023-04-21 08:15:06 -0300

    hechos 1 2 3 4 7

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import {concatAndReverseString} from './manejo-strings.js'
+console.log("EJ 01: ");
 let c1 = "Escuela"
 let c2 = "ORT"
 console.log(`Cadena 1: ${c1} y Cadena 2: ${c2}`)
 console.log(`Cadena invertida: ${concatAndReverseString(c1,c2)}`)
 
-import {PI,suma,resta,multiplicar,division} from './matematica.js'
 
+import {PI,suma,resta,multiplicar,division} from './matematica.js'
+console.log("EJ 02: ");
 let n1 = 10
 let n2 = 5
 console.log(`El valor de PI es ${PI}`)
@@ -14,16 +16,20 @@ console.log(resta(n1,n2))
 console.log(multiplicar(n1,n2))
 console.log(division(n1,n2))
 
+
 import { Alumno } from './Alumno.js'
+console.log("EJ 03: ");
 const alumno1 = new Alumno("Lean Galanterni", 24345543)
 const alumno2 = new Alumno("Lautaro Kaliszczak", 46916288)
 alumno1.mostrar()
 alumno2.mostrar()
 
 import { copiar } from './copyFile.js'
+console.log("EJ 04: ");
 
 copiar("prueba1.txt","prueba2.txt")
 
 import { buscarMoneda } from './country-to-currency.js'
+console.log("EJ 07: ");
 
 buscarMoneda('AR')
